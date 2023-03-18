e4905acb043f28ba6a069230787ceaff3e53df23
Lipon Roy <liponbdbd@gmail.com>
2023-03-18 23:45:44 +0600

    added react frontend

---
+++ clint/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<div className=' h-screen w-full flex flex-col items-center justify-center'>
+				<span className=' text-4xl'>Frontend started by Lipon Roy </span>
+			</div>
+		</>
+	);
+}
+
+export default App;
